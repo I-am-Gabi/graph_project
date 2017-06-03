43491442ed3712861b93a0b36d457e0ae5c96287
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-03 18:08:05 -0300

    tentando rodar o dijikstra ?

--- algo/dijkstra.py
+++ algo/dijkstra.py
@@ -1,8 +1,13 @@
 import networkx as nx
 import numpy as np
+# from engine.graph import Graph
 
 
 def dijkstra(matrix):
     A = np.array(matrix)
     G = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
     print(nx.dijkstra_path(G, 0, 3))
+
+# grafo = Graph()
+# print(grafo.repository.data)
+# dijkstra(grafo.repository.data[1])

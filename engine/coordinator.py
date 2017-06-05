ef5d32cdf803d7bd88ead7097888b609374101c2
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-05 19:28:45 -0300

    coordinator = main menu executável

--- engine/coordinator.py
+++ engine/coordinator.py
@@ -4,6 +4,7 @@ import numpy as np
 sys.path.append('../')
 
 from algo.dijkstra import dijkstra
+from algo.gargalo import gargalo
 
 from engine.repository import Repository
 from engine.wsm import wsm
@@ -29,15 +30,37 @@ def print_matrix(filename, data):
             # outfile.write('# New slice\n')
 
 if __name__ == '__main__':
-    r = Repository()
-    r.build()
+    while True:
+        try:
+            escolha = eval(input('Escolha uma ação. Opções disponíveis:\n1. Caminho mínimo\n2. Identificar gargalos\n'))
+            r = Repository()
+            r.build()
+            # dijikstra
+            if escolha == 1:
+                normalize(r)
 
-    normalize(r)
+                print_matrix('../output/connections.log', r.data[0].connections)
+                print_matrix('../output/normalize.log', r.data[0].connections_normalize)
 
-    print_matrix('../output/connections.log', r.data[0].connections)
-    print_matrix('../output/normalize.log', r.data[0].connections_normalize)
+                result = wsm(r)
 
-    result = wsm(r)
+                print(dijkstra(result, 0, 3))
+                break
+            # gargalo
+            elif escolha == 2:
+                try:
+                    escolha = eval(input('Escolha um critério a ser analisado. Opções disponíveis:\n1. Banda\n2. '
+                                         'Distância\n'))
+                    if escolha != 1 and escolha != 2:
+                        raise NameError
 
-    print(dijkstra(result, 0, 3))
+                    index_matrix = escolha - 1
 
+                    print(gargalo(r.data[index_matrix]))
+                    break
+                except NameError:
+                    raise NameError
+            else:
+                raise NameError
+        except NameError:
+            print('Opção inválida!')

a651c8a5491d39b2438ebdac241440e44fd350cf
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-05 20:35:54 -0300

    fix module problem

--- engine/coordinator.py
+++ engine/coordinator.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 import numpy as np
+
+sys.path.append('../')
+
 from algo.dijkstra import dijkstra
 from algo.gargalo import gargalo
 
 from engine.repository import Repository
 from engine.wsm import wsm
 from engine.normalize import normalize
-
-sys.path.append('../')
-
 
 def print_matrix(filename, data):
     # Write the array to disk
